3b5e59d985612524f7953074b0b75caf376f7c83
Reisyanapr <reisyanurisma2017@gmail.com>
2022-12-16 13:23:41 +0700

    jadwal_sholat, dzikir

---
+++ app/src/main/java/com/thoriq/islampro/DzikirSughra.kt
@@ -0,0 +1,11 @@
+package com.thoriq.islampro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DzikirSughra : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dzikir_sughra)
+    }
+}
---
+++ app/src/main/java/com/thoriq/islampro/JadwalSholatActivity.kt
@@ -0,0 +1,11 @@
+package com.thoriq.islampro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JadwalSholatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jadwal_sholat)
+    }
+}
